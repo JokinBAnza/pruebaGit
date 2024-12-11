7ac58657ff6906f01dcea9e3476aa0e10a111940
Arkaitz Amostegui <ikdge@plaiaundi.net>
2024-12-11 11:57:00 +0100

    Segundo

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hola Jokin, soy Arritxu!");
         System.out.println("Hola Jokin");
+        System.out.println("Se me olvidaba decirte");
     }
 }
