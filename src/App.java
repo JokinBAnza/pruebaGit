d7828333b618f72bf84b50a30c0a3cd28924f6f0
Jokin Berridi <ikdgg@plaiaundi.net>
2024-12-11 10:22:51 +0100

    primerCommit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hola, Rama Nueva!");
+        System.out.println("Hola, Arritxu!");
+        System.out.println("Soy Jokin");
     }
 }
